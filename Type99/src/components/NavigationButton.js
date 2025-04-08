3da1e84883beef27cd3d53edf510ab062c6ca457
xxstraky <alexluong1016@gmail.com>
2025-04-08 12:28:22 -0700

    changed button color for clarity

--- Type99/src/components/NavigationButton.js
+++ Type99/src/components/NavigationButton.js
@@ -3,8 +3,8 @@ export class NavigationButton {
     constructor(scene, x, y, text, callback, style = {}) {
         this.button = scene.add.text(x, y, text, {
             fontSize: '32px',
-            color: '#ffffff',
-            backgroundColor: '#000000',
+            color: '#000000',
+            backgroundColor: '#ffffff',
             padding: {x : 20, y : 10},
             ...style,
         }).setOrigin(0.5).setInteractive();
